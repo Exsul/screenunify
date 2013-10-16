85823ea7220fa38c547d80ddd882b1aa6b829b18
Kirill Berezin <enelar@develop-project.ru>
2013-10-16 19:25:28 +0400

    Loading resource by id automaticly

--- src/bsiapi/storage/ResourceStorage.java
+++ src/bsiapi/storage/ResourceStorage.java
@@ -4,7 +4,15 @@ public class ResourceStorage<T> extends GenericStorage<T> {
     @Override
     public T get( final Integer id ) {
         T cache = super.get(id);
-        assert(cache != null); // TODO: Make resource autoload
+        if (cache == null)
+            cache = super.put(id, loadById(id));
+        assert(cache != null);
         return cache;
+    }
+
+    public T loadById( final int id ) {
+       throw new UnsupportedOperationException(
+               "Resource load generic method is obvious, you should overload " +
+               "ResourceStorage<T> loadById with template specification");
     }
 }
