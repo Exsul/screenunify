6e4584620935e3850c58399e92399f7008e5f937
Kirill Berezin <enelar@develop-project.ru>
2013-10-16 20:43:16 +0400

    Simple bitmaps with distortion container

---
+++ src/net/exsul/screenunify/bitmap/BitmapContainer.java
@@ -0,0 +1,23 @@
+package net.exsul.screenunify.bitmap;
+
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import net.exsul.screenunify.storage.GenericStorage;
+
+public class BitmapContainer<DistortionInfo> extends GenericStorage<BitmapStorage, DistortionInfo> {
+    private Resources resources;
+
+    public BitmapContainer(Resources _resources) {
+        resources = _resources;
+    }
+
+    Bitmap get( final DistortionInfo d, final Integer id ) {
+        BitmapStorage bs = get(d);
+        return bs.get(id);
+    }
+
+    @Override
+    public BitmapStorage onKeyMiss( final DistortionInfo d ) {
+        return new BitmapStorage(resources);
+    }
+}
