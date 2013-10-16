ce7bd8a464fe2cdf9980d46800b71bc0a87355ca
Kirill Berezin <enelar@develop-project.ru>
2013-10-16 19:27:21 +0400

    Refactor load to more simple name

--- src/bsiapi/storage/ResourceStorage.java
+++ src/bsiapi/storage/ResourceStorage.java
@@ -5,12 +5,12 @@ public class ResourceStorage<T> extends GenericStorage<T> {
     public T get( final Integer id ) {
         T cache = super.get(id);
         if (cache == null)
-            cache = super.put(id, loadById(id));
+            cache = super.put(id, load(id));
         assert(cache != null);
         return cache;
     }
 
-    public T loadById( final int id ) {
+    public T load( final int id ) {
        throw new UnsupportedOperationException(
                "Resource load generic method is obvious, you should overload " +
                "ResourceStorage<T> loadById with template specification");
