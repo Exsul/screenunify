0ee410990b2005bcc77aa59e16b94cad01d4729b
Kirill Berezin <enelar@develop-project.ru>
2013-10-18 01:25:30 +0400

    Comfortable rotation use

--- src/net/exsul/screenunify/resource/distortion/RotationDistortion.java
+++ src/net/exsul/screenunify/resource/distortion/RotationDistortion.java
@@ -4,6 +4,16 @@ public class RotationDistortion extends Distortion {
     public float value;
 
     public RotationDistortion(final float angle) {
+        update(angle);
+    }
+
+    public RotationDistortion update( final float angle ) {
         value = angle;
+        return this;
+    }
+
+    public <T> T apply( final T obj, final float angle ) {
+        update(angle);
+        return apply(obj);
     }
 }
