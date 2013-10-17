6a188fb7ce7e15ea2b41a94ea8c6e430bf426002
Kirill Berezin <enelar@develop-project.ru>
2013-10-17 15:07:02 +0400

    Default device distortion

--- src/net/exsul/screenunify/distortion/info/RotationDistortionInfo.java
+++ src/net/exsul/screenunify/distortion/info/RotationDistortionInfo.java
@@ -2,4 +2,8 @@ package net.exsul.screenunify.distortion.info;
 
 public class RotationDistortionInfo extends DistortionInfo {
     public float value;
+
+    public RotationDistortionInfo( final float angle ) {
+        value = angle;
+    }
 }
--- src/net/exsul/screenunify/manager/BitmapsCore.java
+++ src/net/exsul/screenunify/manager/BitmapsCore.java
@@ -1,7 +1,14 @@
 package net.exsul.screenunify.manager;
 
 import android.content.res.Resources;
+import android.graphics.Bitmap;
 import net.exsul.screenunify.bitmap.BitmapContainer;
+import net.exsul.screenunify.bitmap.distortion.RotationDistortionBitmap;
+import net.exsul.screenunify.distortion.Distortion;
+import net.exsul.screenunify.distortion.NullDistortion;
+import net.exsul.screenunify.distortion.info.DistortionInfo;
+import net.exsul.screenunify.distortion.info.RotationDistortionInfo;
+import net.exsul.screenunify.metrics.Metrics;
 
 public class BitmapsCore {
     private BitmapContainer db;
@@ -23,5 +30,30 @@ public class BitmapsCore {
         if (core == null)
             core = new BitmapsCore();
         return core;
+    }
+
+    @Deprecated
+    private Metrics screenMetrics;
+
+    @Deprecated
+    public void initMetrics( final Metrics metrics ) {
+        screenMetrics = metrics;
+    }
+
+    @Deprecated
+    public Distortion<Bitmap> defaultDistortion() {
+        assert(screenMetrics != null);
+        Distortion<Bitmap> ret = new NullDistortion<Bitmap>();
+
+        /* TODO: Scaling bitmap as you want
+        int newW = (int)(screenMetrics.w() * nw);
+        int newH = (int)(bitmapInfoDeprecated.h() * screenMetrics.w() * nw / bitmapInfoDeprecated.w());
+
+        ret.next(new ScaleDistortionBitmap())
+        scaledBitmap = Bitmap.createScaledBitmap(bitmapInfoDeprecated.bitmap(), newW, newH, true);
+        */
+        if (screenMetrics.w() < screenMetrics.h())
+            ret.next(new RotationDistortionBitmap(new RotationDistortionInfo(90)));
+        return ret;
     }
 }
